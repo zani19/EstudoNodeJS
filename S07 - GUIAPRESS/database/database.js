660d3a400b227904d21693444323b30226e79fc2
Thiago Zani <thiago.zani@uol.com.br>
2022-12-20 10:42:14 -0300

    Projeto GuiaPress

---
+++ S07 - GUIAPRESS/database/database.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+
+const connection = new Sequelize('guiapress','root', '',{
+    host:'127.0.0.1',
+    port:'3306',
+    dialect:'mysql'
+});
+
+module.exports = connection;
