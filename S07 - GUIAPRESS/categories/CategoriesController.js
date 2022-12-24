ea353de1b29b0f486ace5b7c7b895eeb35744cf2
Thiago Zani <thiago.zani@uol.com.br>
2022-12-24 18:38:02 -0300

    paginas home e menus interativos por categorias

--- S07 - GUIAPRESS/categories/CategoriesController.js
+++ S07 - GUIAPRESS/categories/CategoriesController.js
@@ -57,7 +57,6 @@ router.get("/admin/categories/edit/:id", (req,res) => {
         res.redirect("/admin/categories");
     }
 
-
     Category.findByPk(id).then(category => {
         if(category != undefined){            
             res.render("admin/categories/edit", {category: category});
@@ -66,7 +65,20 @@ router.get("/admin/categories/edit/:id", (req,res) => {
         }
     }).catch(erro => {
         res.redirect("/admin/categories");
-    })
-})
+    });
+});
+
+router.post("/categories/update", (req,res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    });
+});
 
 module.exports = router;
--- S07 - GUIAPRESS/database/database.js
+++ S07 - GUIAPRESS/database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('guiapress','root', '',{
     host:'127.0.0.1',
     port:'3306',
-    dialect:'mysql'
+    dialect:'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
